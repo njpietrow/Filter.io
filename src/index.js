5fd3f4546ba36fb32f310f754313d3da588925c9
Nick Pietrow <njpietrow@gmail.com>
2022-02-24 22:48:18 -0800

    added index.html structure and base formatting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
   let canvas = document.getElementById('game-canvas');
   let ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth*(.8);
+  canvas.height = canvas.width * (5/7);
 
   let background = new Image();
   background.src = "https://thediscerningcat.com/wp-content/uploads/2021/09/british-short-hair-chincilla-up-close.jpg";
 
   background.onload = function(){
-    ctx.drawImage(background,0,0,700,500);   
+    ctx.drawImage(background,0,0,canvas.width, canvas.height);   
   }
   // ctx.fillStyle = "blue";
   // ctx.fillRect(0, 0, canvas.width, canvas.height); 
