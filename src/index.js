52ba8fbc71904f2e72ff13ada91d1ce42e87cdfb
Nick Pietrow <njpietrow@gmail.com>
2022-02-26 20:40:21 -0800

    added demo code to print video to canvas and display face landmarks

--- src/index.js
+++ src/index.js
@@ -1,53 +1,68 @@
-//Add a script for the filter class
-//add a script for the face mesh
 import VideoTools from './scripts/video_tools';
-import {FaceMesh} from './../node_modules/@mediapipe/face_mesh/face_mesh'
-// const faceMesh = require('@tensorflow-models/facemesh');
+//Add a script for the filter class
 //Add a script(s) for button effects
 
 window.addEventListener('DOMContentLoaded', () => {
-  let canvas = document.getElementById('game-canvas');
-  let ctx = canvas.getContext('2d');
-  canvas.width = window.innerWidth * (.8);
-  if (canvas.width > 700) canvas.width = 700;
-  canvas.height = canvas.width * (720/1280);
-
-  ctx.fillStyle = "rgba(255, 200, 150, 0.3)";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  ctx.beginPath();
-  ctx.arc(400, 90, 25, 0, 2 * Math.PI);
-  ctx.strokeStyle = 'blue';
-  ctx.lineWidth = 10;
-  ctx.stroke();
-  ctx.fillStyle = 'pink';
-  ctx.fill();
-
-
-  // // let background = new Image();
-  // // background.src = "https://thediscerningcat.com/wp-content/uploads/2021/09/british-short-hair-chincilla-up-close.jpg";
-
-  // // background.onload = function(){
-  // //   ctx.drawImage(background,0,0,canvas.width, canvas.height);   
-  // // }
-
   
-  const video = document.querySelector("#video");
-  VideoTools.startVideo(video);
+  // const canvasElement = document.getElementsByClassName('output_canvas')[0];
+  // const canvasCtx = canvasElement.getContext('2d');
+  
+  const videoElement = document.querySelector("#video");
+  const canvasElement = document.querySelector("#game-canvas")
+  const canvasCtx = canvasElement.getContext('2d');
+  
+  canvasElement.width = window.innerWidth * (.8);
+  if (canvasElement.width > 700) canvasElement.width = 700;
+  canvasElement.height = canvasElement.width * (720/1280);
+  
+  canvasCtx.fillStyle = "rgba(255, 200, 150, 0.3)";
+  // canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+  
+  let drawCircle = function(){
+    canvasCtx.beginPath();
+    canvasCtx.arc(400, 90, 25, 0, 2 * Math.PI);
+    canvasCtx.strokeStyle = 'blue';
+    canvasCtx.lineWidth = 10;
+    canvasCtx.stroke();
+    canvasCtx.fillStyle = 'pink';
+    canvasCtx.fill();
+  }
+  // drawCircle();
+  
+  
+  //interval loop to redraw all applicable filters
+    //redraw canvas border
 
-  video.style.opacity = 0;
+  VideoTools.startVideo(videoElement);
 
-  video.oncanplay = function() {
-    VideoTools.fade(video);
+  videoElement.style.opacity = 0;
+
+  videoElement.oncanplay = function() {
+    VideoTools.fade(videoElement); //video is playing in the background behind the canvas.
+    VideoTools.fade(canvasElement)
   };
 
-  async function drawVideo(){
-    // Draw the video stream into our screen
-    ctx.drawImage(video, 0, 0);
-    // Call self again
-    requestAnimationFrame(drawVideo);
-}
-
+  function onResults(results) {
+    console.log(results)
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+        results.image, 0, 0, canvasElement.width, canvasElement.height);
+    if (results.multiFaceLandmarks) {
+      for (const landmarks of results.multiFaceLandmarks) {
+        drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION, {color: '#C0C0C070', lineWidth: 1});
+        drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, {color: '#FF3030'});
+        drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, {color: '#FF3030'});
+        drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_IRIS, {color: '#FF3030'});
+        drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYE, {color: '#30FF30'});
+        drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, {color: '#30FF30'});
+        drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_IRIS, {color: '#30FF30'});
+        drawConnectors(canvasCtx, landmarks, FACEMESH_FACE_OVAL, {color: '#E0E0E0'});
+        drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, {color: '#E0E0E0'});
+      }
+    }
+    canvasCtx.restore();
+  }
 
   const faceMesh = new FaceMesh({locateFile: (file) => {
     return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
@@ -58,31 +73,22 @@ window.addEventListener('DOMContentLoaded', () => {
     minDetectionConfidence: 0.5,
     minTrackingConfidence: 0.5
   });
+  faceMesh.onResults(onResults);
+  // console.log(faceMesh);
 
 
-  // const camera = new Camera(videoElement, {
-  //   onFrame: async () => {
-  //     await faceMesh.send({image: videoElement});
-  //   },
-  //   width: 1280,
-  //   height: 720
-  // });
-  // camera.start();
+  const camera = new Camera(videoElement, {
+    onFrame: async () => {
+      await faceMesh.send({image: videoElement});
+    },
+    width: 1280,
+    height: 720
+  });
+  camera.start();
 
 
 
-  console.log(faceMesh);
 
-
-  //interval loop to redraw all applicable filters
-    //redraw canvas border
-
-  // video.addEventListener( "loadedmetadata", function (e) {
-  //   var width = this.videoWidth,
-  //       height = this.videoHeight;
-  //   console.log(width)
-  //   console.log(height)
-  // }, false ); 
 
 
 });
